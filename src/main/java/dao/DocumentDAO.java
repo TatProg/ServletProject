06fff60c9bea53fe57f98c6dcd6d099ca886847a
Aydar <nasyrov.aydar13@gmail.com>
2020-05-05 21:12:19 +0300

    added .doc upload func

--- src/main/java/dao/DocumentDAO.java
+++ src/main/java/dao/DocumentDAO.java
@@ -1,20 +1,43 @@
 package dao;
 
+import java.io.*;
 import java.sql.*;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 
 public class DocumentDAO {
     public void saveDocument(StringBuilder data) {
+        //enter here your db table
+        String sqlString = "INSERT INTO text (id, text) VALUES(?, ?)";
+        String mainString = null;
+        String secString = null;
+
         try {
             Connection connection = ConnectionDB.getDBConnection();
-            PreparedStatement statement = connection.prepareStatement("insert into documents (data) values (?)");
-            statement.setString(1, data.toString());
-            statement.executeUpdate();
 
-            statement.close();
-        } catch (SQLException e) {
+            String nameString = data.toString().replaceAll("\\.", "|");
+            //trying to find .doc
+            if (nameString.split("\\|")[1].equals("doc")){
+                FileInputStream fis = new FileInputStream(nameString);
+                HWPFDocument document = new HWPFDocument(fis);
+                WordExtractor extractor = new WordExtractor(document);
+                String[] fileData = extractor.getParagraphText();
+                for (int i = 0; i < fileData.length; i++) {
+                    if (fileData[i] != null)
+                        mainString+= fileData[i] +" ";
+                }
+            } else {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(nameString)));
+                while ((secString = bufferedReader.readLine()) != null) {
+                    mainString += secString + " ";
+                }
+            }
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlString);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, mainString);
+            preparedStatement.executeUpdate();
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
-
-
         }
     }
 }
